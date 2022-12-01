d941e5a5b7bd30ee2a261210badb482412cb8193
s0620014 <s0620014@iwate-u.local>
2022-12-01 17:40:20 +0900

    update: プレイヤー\n向きを変えられるようになったがバグもある\n手元に保存できないのでプッシュする\n

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -10,12 +10,24 @@ public class Player : MonoBehaviour {
 
 	public float ForceHorizontalMove = 30;
 	public float ForceAscent = 5;
+
+	public float MaxSpeedTurnY = 2f;
+	public float SpeedTurnIncreaseRateY = 3f;
+	public float MaxSpeedTurnX = 0.8f;
+	public float SpeedTurnIncreaseRateX = 0.4f;
+
+	public Camera MainCamera;
+
+	private Rigidbody rb;
+	//private Rigidbody rbCamera;
 	
 	void Start() {
-		
+		rb = gameObject.GetComponent<Rigidbody>();
+		//rbCamera = MainCamera.gameObject.GetComponent<Rigidbody>();
 	}
 
 	void Update() {
+		ChangeAngle();
 		Move();
 	}
 
@@ -25,11 +37,8 @@ public class Player : MonoBehaviour {
 	}
 
 	private void Move() {
-		var rb = gameObject.GetComponent<Rigidbody>();
-
 		// 入力を元に力を加える
 		var ForceH = new Vector3(0f, 0f, 0f);
-		var ForceV = new Vector3(0f, 0f, 0f);
 		if (Input.GetKey(KeyCode.E)) ForceH.z = ForceHorizontalMove;
 		if (Input.GetKey(KeyCode.F)) ForceH.x = ForceHorizontalMove;
 		if (Input.GetKey(KeyCode.D)) ForceH.z = -1f * ForceHorizontalMove;
@@ -50,10 +59,55 @@ public class Player : MonoBehaviour {
 			);
 		}
 
-		rb.AddForce(ForceH);
+		// 力を加える
+		rb.AddForce(transform.rotation * ForceH);
+		//Debug.Log(transform.forward);			// オブジェクトのz軸方向を表すワールド単位ベクトル（-0.35, 0.00, 0.94 とか）
+		//Debug.Log(rb.transform.forward);		// 上記に同じ
+		//Debug.Log(Vector3.forward);			// 0,0,1で固定（ローカル座標値であるため）
 	}
 
 	private void Jump() {
 		gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, ForceAscent, 0f), ForceMode.Impulse);
 	}
+
+	private void ChangeAngle() {
+		var	  mousePos	 = MainCamera.ScreenToViewportPoint(Input.mousePosition);
+		float turnSpeedY = 0f;
+		float absAngleY	 = Mathf.Abs(mousePos.x - 0.5f);    // 0.0 ～ 0.5
+		float turnSpeedX = 0f;
+		float absAngleX	 = Mathf.Abs(mousePos.y - 0.5f);    // 0.0 ～ 0.5
+
+		if (absAngleY > 0.02) {
+			turnSpeedY = (absAngleY * SpeedTurnIncreaseRateY) * MaxSpeedTurnY;
+			if (mousePos.x < 0.5)
+				turnSpeedY *= -1;
+			if (turnSpeedY > MaxSpeedTurnY)
+				turnSpeedY = MaxSpeedTurnY;
+			if (turnSpeedY < -1 * MaxSpeedTurnY)
+				turnSpeedY = -1 * MaxSpeedTurnY;
+		}
+		if (absAngleX > 0.02) {
+			turnSpeedX = (absAngleX * SpeedTurnIncreaseRateY) * MaxSpeedTurnY;
+			if (mousePos.y > 0.5)
+				turnSpeedX *= -1;
+			if (turnSpeedX > MaxSpeedTurnX)
+				turnSpeedX = MaxSpeedTurnX;
+			if (turnSpeedX < -1 * MaxSpeedTurnX)
+				turnSpeedX = -1 * MaxSpeedTurnX;
+
+			//var angle = rbCamera.transform.eulerAngles.x;
+			//if (   angle < 360 - 70 && angle > 180 && turnSpeedX > 0		// 下回転を止める
+			//	|| angle > 70 && angle < 180 && turnSpeedX < 0)		// 上回転を止める
+			//	turnSpeedX = 0f;
+			float angle = MainCamera.transform.rotation.x;
+			if (   angle < -0.3 && turnSpeedX <	0		// 上回転を止める
+				|| angle > 0.2 && turnSpeedX > 0)		// 下回転を止める
+				turnSpeedX = 0f;
+		}
+		rb.angularVelocity = new Vector3(0f, turnSpeedY, 0f);
+		MainCamera.transform.Rotate(new Vector3(turnSpeedX, 0f, 0f));
+		//rbCamera.angularVelocity = new Vector3(turnSpeedX, 0f, 0f);
+		//Debug.Log("angle: " + rbCamera.transform.eulerAngles.x);
+		Debug.Log("angle" + MainCamera.transform.rotation.x);
+    }
 }
