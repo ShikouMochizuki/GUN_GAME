c713edd63f7659f9847e9decc8f8c87e6a6a3ec4
s0620014 <s0620014@iwate-u.local>
2022-12-17 16:46:21 +0900

    update: Gun 弾速に自機の速度を足し合わせるようにした

--- Assets/Script/Gun.cs
+++ Assets/Script/Gun.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public Transform bulletSpawnPoint;
-    public GameObject bulletPrefab;
-    public float bulletSpeed = 10;
+	public Transform bulletSpawnPoint;
+	public GameObject bulletPrefab;
+	public float bulletSpeed = 10;
+	public GameObject Player;
+	private Rigidbody rb;
 
-    void Update()
-    {
-        //if (Input.GetKeyDown(KeyCode.Space))
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.E))
-        {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
-        }
-    }
+	private void Start() {
+		rb = Player.GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		//if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.E))
+		{
+			var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+			bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed + rb.velocity;
+		}
+	}
 }
